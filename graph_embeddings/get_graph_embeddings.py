5182447c3235be33de933057b05b8a72acb9d7a2
Bijan Haney <bijan.haney@consensys.net>
2019-01-10 15:28:40 -0500

    adds plotting notebook

--- graph_embeddings/get_graph_embeddings.py
+++ graph_embeddings/get_graph_embeddings.py
@@ -2,18 +2,21 @@
 # output: list of tuples of form (node_name, node_embedding)
 import argparse, pickle, os
 from multigraph import Multigraph
-from rgcn_embeddings import rgcn_embeddings
+from embedding_models.rgcn_embeddings import rgcn_embeddings
 
 def save_object(obj, filename):
     with open(filename, 'wb') as output:  # Overwrites any existing file.
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
-def get_graph_embeddings(list_of_files):
+def get_graph(list_of_files):
     # create multigraph from list of connections
     graph = Multigraph()
     for f in list_of_files:
         graph.read_csv(f)
-    embeddings = rgcn_embeddings(graph)
+    return graph
+
+def get_graph_embeddings(graph,target_csv=None):
+    embeddings = rgcn_embeddings(graph,target_csv)
     return zip(graph.node_names, embeddings)
     
 if __name__ == "__main__":
@@ -21,6 +24,8 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-d", "--dir", help="path of directory of csv files.")
     group.add_argument("-f", "--files",action='append', help="csv files with connections, separated by commas.")
+    parser.add_argument("-t", "--target", help="csv file with targets for training.")
+    parser.add_argument("-n", "--name", help="name for output files.", required=True)
     args = parser.parse_args()
     list_of_files = list()
     if args.files:
@@ -30,9 +35,16 @@ if __name__ == "__main__":
     print('Using the following CSV files:')
     for i in list_of_files:
         print('  '+i)
+    target_csv = None
+    if args.target:
+        target_csv = args.target
+    name = args.name
     #BEGIN
-    embeddings = get_graph_embeddings(list_of_files)
+    graph = get_graph(list_of_files)
+    embeddings = get_graph_embeddings(graph, target_csv)
     if not os.path.isdir(os.path.join(os.getcwd(),'results')):
         os.mkdir(os.path.join(os.getcwd(),'results'))
-    save_object(embeddings, os.path.join(os.getcwd(),'results','embeddings.pkl'))
+    save_object(graph, os.path.join(os.getcwd(),'results',name+'_graph.pkl'))
+    print("Saved graph to results")
+    save_object(embeddings, os.path.join(os.getcwd(),'results',name+'_embeddings.pkl'))
     print("Saved embeddings to results")
